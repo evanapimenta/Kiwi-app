e1e2fd4023d1afbac404338063372d37e3bce853
Evana Pimenta <evana_pimenta@live.com>
2024-07-25 00:18:37 -0300

    Create Album, Artist, Genre and Track classes.

---
+++ Kiwi-app/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiwi_app
+{
+    internal class Artist
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Kiwi-app/Track.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiwi_app
+{
+    internal class Track
+    {
+        public string Title { get; }
+        public Artist Artist { get; }
+        public Album Album { get; }
+        public TimeSpan TrackDuration { get; }
+        public List<Genre> TrackGenres { get; }
+
+        public string SongInfo =>
+            $"Song Info:\n" +
+            $"Title: {Title}\n" +
+            $"Artist: {Artist.Name}\n" +
+            $"Album: {Album.Title}\n" +
+            $"Genre: {string.Join(", ", TrackGenres)}\n)" +
+            $"Length: {TrackDuration}";
+
+        public Track(string title, Artist artist, Album album, TimeSpan trackDuration)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Artist = artist ?? throw new ArgumentNullException(nameof(artist));
+            Album = album ?? throw new ArgumentNullException(nameof(album));
+            TrackGenres = TrackGenres ?? throw new ArgumentNullException(nameof(TrackGenres));
+           
+            if (trackDuration != TimeSpan.Zero) TrackDuration = trackDuration; else throw new ArgumentNullException(nameof(TrackGenres));
+        }
+    }
+}
