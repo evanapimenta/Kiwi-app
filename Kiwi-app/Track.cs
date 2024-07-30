7b3416e3d6d5f39146935e1a3e41b9b3d01b8094
Evana Pimenta <evana_pimenta@live.com>
2024-07-30 13:59:28 -0300

    Refactor Album, Genre and Track classes.

--- Kiwi-app/Track.cs
+++ Kiwi-app/Track.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace Kiwi_app
 {
-    internal class Track
+    /// <summary>
+    /// Represents a track/song in the system.
+    /// </summary>
+    /// <param name="Title">The track's title.</param>
+    /// <param name="Artist">The artist associated with the track.</param>
+    /// <param name="Album">The album associated with the track.</param>
+    /// <param name="Duration">The track's length.</param>
+    /// <param name="TrackGenres">A list of one of more genres that may be associated with a track.</param>
+    internal class Track(string Title, Artist Artist, Album Album, TimeSpan Duration, List<Genre> TrackGenres)
     {
-        public string Title { get; }
-        public Artist Artist { get; }
-        public Album Album { get; }
-        public TimeSpan TrackDuration { get; }
-        public List<Genre> TrackGenres { get; }
+        public string Title { get; } = Title ?? throw new ArgumentNullException(nameof(Title), "Track title cannot be null");
+        public Artist Artist { get; } = Artist ?? throw new ArgumentNullException(nameof(Artist), "Artist cannot be null");
+        public Album Album { get; } = Album ?? throw new ArgumentNullException(nameof(Album), "Album cannot be null");
+        public TimeSpan Duration { get; } = Duration;
+        public List<Genre> TrackGenres { get; } = TrackGenres ?? throw new ArgumentNullException(nameof(TrackGenres), "Genres cannot be null");
+        public List<double> TrackRatings { get; } = new();
+        public double AverageRating => TrackRatings.Count == 0 ? 0 : TrackRatings.Average();
 
-        public string SongInfo =>
+        public string TrackInfo =>
             $"Song Info:\n" +
             $"Title: {Title}\n" +
             $"Artist: {Artist.Name}\n" +
             $"Album: {Album.Title}\n" +
             $"Genre: {string.Join(", ", TrackGenres)}\n)" +
-            $"Length: {TrackDuration}";
-
-        public Track(string title, Artist artist, Album album, TimeSpan trackDuration)
-        {
-            Title = title ?? throw new ArgumentNullException(nameof(title));
-            Artist = artist ?? throw new ArgumentNullException(nameof(artist));
-            Album = album ?? throw new ArgumentNullException(nameof(album));
-            TrackGenres = TrackGenres ?? throw new ArgumentNullException(nameof(TrackGenres));
-           
-            if (trackDuration != TimeSpan.Zero) TrackDuration = trackDuration; else throw new ArgumentNullException(nameof(TrackGenres));
-        }
+            $"Length: {Duration}";
     }
 }
