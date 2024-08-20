58c6b0ce2cc4fcd700b31f177d0e2dc1a14a250b
Evana Pimenta <evana_pimenta@live.com>
2024-08-20 13:11:13 -0300

    Refactor User Management and Password Handling
    - Added `IPasswordHasher` class for password hashing using PBKDF2.
    - Integrated `IPasswordHasher` in the `User` class for secure password storage.
    - Updated `User` class to hash passwords in the primary constructor.
    - Refactored class structure and applied best practices for encapsulation.
    - Organized project into structured folders according to best practices.
      - Created `Models/UserManagement` for password hashing and user management related classes.
      - Ensured proper encapsulation for class fields and properties.

---
+++ Kiwi-app/Models/Music/Track.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiwi_app.Models.Music;
+
+/// <summary>
+/// Represents a track/song in the system.
+/// </summary>
+/// <param name="Title">The track's title.</param>
+/// <param name="Artist">The artist associated with the track.</param>
+/// <param name="Album">The album associated with the track.</param>
+/// <param name="Duration">The track's length.</param>
+/// <param name="TrackGenres">A list of one or more genres that may be associated with a track.</param>
+
+internal class Track(string Title, TimeSpan Duration)
+{
+    private Album _album;
+    private Artist _artist;
+
+    public string Title { get; } = Title ?? throw new ArgumentNullException(nameof(Title), "Track title cannot be null");
+    public Artist Artist
+    {
+        get => _artist;
+        set => _artist = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public Album Album
+    {
+        get => _album;
+        set => _album = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public TimeSpan Duration { get; } = Duration;
+    public List<Genre> TrackGenres { get; } = new();
+    public List<double> TrackRatings { get; } = new();
+    public double AverageRating => TrackRatings.Count == 0 ? 0 : TrackRatings.Average();
+
+    public string TrackInfo =>
+        $"Song Info:\n" +
+        $"Title: {Title}\n" +
+        $"Artist: {Artist.Name}\n" +
+        $"Album: {Album.Title}\n" +
+        $"Genre: {string.Join(", ", TrackGenres)}\n)" +
+        $"Length: {Duration}";
+}
+
--- Kiwi-app/Program.cs
+++ Kiwi-app/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kiwi_app.Models.Music;
+
+Dictionary<string, Artist> artists = new();
+
+//// Create Artist
+Artist newArtist = new("The Cure");
+
+//// Create Album
+Album newAlbum = new(newArtist, "4:13 Dream", 2008);
+
+//// Add Genres to Album
+
+
+//// Create Tracks
+Track track1 = new("Underneath the Stars", new TimeSpan(0, 6, 17));
+Track track2 = new("The Only One", new TimeSpan(0, 3, 57));
+// Add Tracks to Album
+newAlbum.AddTrackToAlbum(track1);
+newAlbum.AddTrackToAlbum(track2);
+
+//// Add Album to Artist's discography
+newArtist.AddAlbumToArtist(newAlbum);
+
+newAlbum.PrintAlbumInfo();
